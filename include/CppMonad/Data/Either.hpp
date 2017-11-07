58078ebca9148cc943189bb59f56402d435c4c31
303248153 <303248153@qq.com>
2017-11-07 15:16:08 +0800

    add Either (not completed)

--- include/CppMonad/Data/Either.hpp
+++ include/CppMonad/Data/Either.hpp
@@ -1,1 +1,80 @@
 #pragma once
+#include <variant>
+#include "../Class/Show.hpp"
+#include "../Class/Functor.hpp"
+#include "../Class/Semigroup.hpp"
+#include "../Class/Monoid.hpp"
+#include "../Class/Apply.hpp"
+#include "../Class/Applicative.hpp"
+#include "../Class/Bind.hpp"
+
+namespace CppMonad {
+	template <class TLeft, class TRight>
+	using Either = std::variant<TLeft, TRight>;
+
+	template <class T>
+	struct PartialEither { };
+
+	template <class TLeft, class TRight>
+	Either<TLeft, TRight> Left(TLeft&& value) {
+		return Either<TLeft, TRight>(std::forward<TLeft>(value));
+	}
+
+	template <class TLeft, class TRight>
+	Either<TLeft, TRight> Right(TRight&& value) {
+		return Either<TLeft, TRight>(std::forward<TRight>(value));
+	}
+
+	template <class TLeft, class TRight>
+	struct Show<Either<TLeft, TRight>> {
+		static std::string show(const Either<TLeft, TRight>& value) {
+			if (value.index() == 0) {
+				return std::string("Left ") + Show<TLeft>::show(std::get<0>(value));
+			}
+			return std::string("Right ") + Show<TRight>::show(std::get<1>(value));
+		}
+	};
+
+	// it's hacky and may not work with nested type,
+	// but c++ isn't support partial template specialization with template template class
+	template <>
+	struct Functor<PartialEither> {
+		template <class TLeft, class TRight, class Func>
+		static auto map(
+			const Func& func,
+			const Either<TLeft, TRight>& a) {
+			Either<TLeft, decltype(func(std::declval<const TRight>()))> b;
+			if (a.index() == 0) {
+				b = std::get<0>(a);
+			} else {
+				b = func(std::get<1>(a));
+			}
+			return b;
+		}
+	};
+
+	template <class TLeft, class TRight, class Func>
+	static auto map(
+		const Func& func,
+		const Either<TLeft, TRight>& a) {
+		return Functor<PartialEither>::map(func, a);
+	}
+
+	template <class TLeft, class TRight>
+	struct Semigroup<Either<TLeft, TRight>> {
+		static Either<TLeft, TRight> append(
+			const Either<TLeft, TRight>& a, const Either<TLeft, TRight>& b) {
+			if (a.index() == 0) {
+				return b;
+			}
+			return a;
+		}
+	};
+
+	template <class TLeft, class TRight>
+	struct Monoid<Either<TLeft, TRight>> {
+		static Either<TLeft, TRight> mempty() {
+			return Either<TLeft, TRight>();
+		}
+	};
+}
--- tests/Cases/TestEither.cpp
+++ tests/Cases/TestEither.cpp
@@ -1,9 +1,57 @@
 #include <iostream>
 #include <cassert>
+#include <CppMonad/Data/String.hpp>
 #include <CppMonad/Data/Either.hpp>
 
 namespace CppMonadTests {
+	using namespace CppMonad;
+
+	void testEitherShow() {
+		auto left = Left<int, String>(1);
+		auto right = Right<int, String>("abc");
+		assert(show(left) == "Left 1");
+		assert(show(right) == "Right abc");
+	}
+
+	void testEitherFunctor() {
+		auto left = Left<int, String>(1);
+		auto right = Right<int, String>("abc");
+		auto mappedLeft = map([](const auto& str) { return str.size(); }, left);
+		auto mappedRight = map([](const auto& str) { return str.size(); }, right);
+		assert(show(mappedLeft) == "Left 1");
+		assert(show(mappedRight) == "Right 3");
+	}
+
+	void testEitherSemigroup() {
+		auto left = Left<int, String>(1);
+		auto right = Right<int, String>("abc");
+		assert(show(append(left, right)) == "Right abc");
+		assert(show(append(right, left)) == "Right abc");
+	}
+
+	void testEitherMonoid() {
+		auto empty = mempty<Either<int, String>>();
+		assert(show(empty) == "Left 0");
+	}
+
+	void testEitherApply() {
+
+	}
+
+	void testEitherApplicative() {
+	}
+
+	void testEitherBind() {
+	}
+
 	void testEither() {
 		std::cout << __func__ << std::endl;
+		testEitherShow();
+		testEitherFunctor();
+		testEitherSemigroup();
+		testEitherMonoid();
+		testEitherApply();
+		testEitherApplicative();
+		testEitherBind();
 	}
 }
